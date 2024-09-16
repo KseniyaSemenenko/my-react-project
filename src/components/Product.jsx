43d143699f70fed7355ec9e9815123d39611ceff
Kseniya Semenenko <165210222+KseniyaSemenenko@users.noreply.github.com>
2024-09-16 18:53:41 +0300

    test

---
+++ src/components/Product.jsx
@@ -0,0 +1,30 @@
+// export default function Product(props) {
+//   return (
+//     <div>
+//       <h2>{props.name}</h2>
+// 	  <img src={props.imgUrl} alt={props.name} width="480" />
+// 	  <p>Price: {props.price} credits</p>
+//     </div>
+//   );
+// };
+
+// export default function Product({ name, imgUrl, price }) {
+//   return (
+//     <div>
+//       <h2>{name}</h2>
+// 	  <img src={imgUrl} alt={name} width="480" />
+// 	  <p>Price: {price} credits</p>
+//     </div>
+//   );
+// };
+
+// export default function BookList({ books })  {
+//   return (
+//     <ul>
+// 	  {books.map((book) => {
+// 		return <li key={book.id}>{book.name}</li>;
+// 	  })}
+// 	</ul>
+//   );
+// };
+
